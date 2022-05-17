f012307fc413c6becd70b0bead2289a071cd1311
CLANDEAU <92865093+CLANDEAU@users.noreply.github.com>
2022-05-17 16:08:19 +0200

    Update urls.py

--- projet/F1project/urls.py
+++ projet/F1project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('formule1/', include('formule1.urls')),
+    path('', include('formule1.urls')),
 ]
